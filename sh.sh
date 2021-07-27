c7563917a57bdd5a94b41a9e8b43b5c1131c452d
obed101 <obedamoako92@gmail.com>
2021-07-27 02:19:07 -0700

    :wrench: Reverted exclusion of %r, also removed unuseful files :broom:

--- sh.sh
+++ sh.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo -e "\e[3m\e[33mProcessing..."
-gcc -Wall -Werror -Wextra -pedantic -Wno-format -std=gnu89 *.c -o kk
-echo "./kk + return"
+gcc -Wall -Werror -Wextra -pedantic -Wno-format -std=gnu89 *.c
